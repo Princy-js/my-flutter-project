bff2f944f6fa963d6e707e12420582045edd4bfc
Princy <princyjames1810@gmail.com>
2023-09-14 11:18:18 +0530

    Flutter project (class)

--- lib/splashPage.dart
+++ lib/splashPage.dart
@@ -15,31 +15,51 @@ class SplashPage extends StatelessWidget {
   Widget build(BuildContext context) {
     //Build context : used to locate the widgets in the widget tree and monitor their actions
     return Scaffold(
-      backgroundColor: Colors.black,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-             Image(image: AssetImage("assets/icons/sonic-generations-icon.png"),
-             width: 100,
-              height: 100,
-             ),
-            // Image(image: NetworkImage("https://cdn1.iconfinder.com/data/icons/space-travel-flat/340/space_astronomy_universe_galaxy_astronaut_cosmonaut_science_spaceman_travel_walk-64.png")),
-            // Icon(
-            //     Icons.cake_sharp,
-            //     size: 80,
-            //     ),
-            Text("My App",
-              style: GoogleFonts.dancingScript(
-                fontSize: 30,
-                color: Colors.pink
+      // backgroundColor: Colors.black,
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors:[
+            Colors.blueAccent,
+            Colors.teal,
+            Colors.green,
+            Colors.greenAccent,
+            Colors.white38,
+            Colors.white
+          ] )
+          // color: Colors.teal
+          // image: DecorationImage(
+          //   fit: BoxFit.cover,
+          //     image: NetworkImage("https://images.unsplash.com/photo-1694528162642-e59afd291f46?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80"))
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+               Image(image: AssetImage("assets/icons/sonic-generations-icon.png"),
+               width: 100,
+                height: 100,
+               ),
+              // Image(image: NetworkImage("https://cdn1.iconfinder.com/data/icons/space-travel-flat/340/space_astronomy_universe_galaxy_astronaut_cosmonaut_science_spaceman_travel_walk-64.png")),
+              // Icon(
+              //     Icons.cake_sharp,
+              //     size: 80,
+              //     ),
+              Text("My Application",
+                style: GoogleFonts.dancingScript(
+                  fontSize: 30,
+                  color: Colors.white,
+                  fontWeight:FontWeight.bold
+                 )
+                // TextStyle(
+                //     fontSize: 40,
+                //     fontStyle: FontStyle.italic,
+                //     color: Colors.yellow)
               )
-              // TextStyle(
-              //     fontSize: 40,
-              //     fontStyle: FontStyle.italic,
-              //     color: Colors.yellow)
-            )
-          ],
+            ],
+          ),
         ),
       ),
     );
