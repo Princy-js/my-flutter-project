a8b505e0b3ac5bf544701e43185528893c43a6c3
Princy <princyjames1810@gmail.com>
2023-09-18 11:43:39 +0530

    Flutter project (class)

---
+++ lib/loginpage stl.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+// void main(){
+//   runApp(
+//       MaterialApp(
+//         home: LoginPage(),
+//       )
+//   );
+// }
+
+class LoginPage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Login Page"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(18.0),
+        child: Column(
+          children: [
+            Image.asset(
+                "assets/icons/sonic-generations-icon.png",
+              width: 130,
+              height: 130,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(18.0),
+              child: Text("My App",
+              style: GoogleFonts.cabinSketch(
+                fontSize: 35,
+                fontWeight: FontWeight.w100,
+                color: Colors.blueAccent
+              ),
+              ),
+            ),
+            TextField(
+              decoration: InputDecoration(
+                hintText: "Username",
+                labelText:"Username",
+                helperText: "Username must be an email id",
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(50))
+                )
+              ),
+            ),
+            SizedBox(height: 20,),
+            TextField(
+              decoration: InputDecoration(
+                  labelText:"Password",
+                  hintText: "Password",
+                  helperText: "Create a strong password",
+                  border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(50))
+                )
+              ),
+            ),
+            SizedBox(height: 20,),
+            ElevatedButton(onPressed: (){} , child: Text("Login")),
+            TextButton(onPressed: (){}, child:Text("Not a user ? login Here!"))
+          ],
+        ),
+      ),
+    );
+  }
+  
+}
--- lib/stf splash.dart
+++ lib/stf splash.dart
@@ -1,34 +1,51 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+
+import 'loginpage stl.dart';
 
 void main() {
   // to run an app
   runApp(MaterialApp(
     //default theme of our flutter app
-    home: SplashPage(), //initial page to be launched while running an app
+    home: Splash2(), //initial page to be launched while running an app
   ));
 }
 
-class SplashPage extends StatelessWidget {
+class Splash2 extends StatefulWidget{
   @override
-  //build functionality: to create our widget tree we use build function
+  State<StatefulWidget> createState() =>Splash2State();
+
+
+
+}
+
+class Splash2State extends State{
+  @override
+  void initState() {
+    Timer(Duration(seconds: 4), () {
+     Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context)=>LoginPage()));
+    });
+    super.initState();
+  }
+  @override
   Widget build(BuildContext context) {
-    //Build context : used to locate the widgets in the widget tree and monitor their actions
     return Scaffold(
       // backgroundColor: Colors.black,
       body: Container(
         decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-              colors:[
-            Colors.blueAccent,
-            Colors.teal,
-            Colors.green,
-            Colors.greenAccent,
-            Colors.white38,
-            Colors.white
-          ] )
+            gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors:[
+                  Colors.blueAccent,
+                  Colors.teal,
+                  Colors.green,
+                  Colors.greenAccent,
+                  Colors.white38,
+                  Colors.white
+                ] )
           // color: Colors.teal
           // image: DecorationImage(
           //   fit: BoxFit.cover,
@@ -38,21 +55,21 @@ class SplashPage extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-               const Image(image: AssetImage("assets/icons/sonic-generations-icon.png"),
-               width: 100,
+              const Image(image: AssetImage("assets/icons/sonic-generations-icon.png"),
+                width: 100,
                 height: 100,
-               ),
+              ),
               // Image(image: NetworkImage("https://cdn1.iconfinder.com/data/icons/space-travel-flat/340/space_astronomy_universe_galaxy_astronaut_cosmonaut_science_spaceman_travel_walk-64.png")),
               // Icon(
               //     Icons.cake_sharp,
               //     size: 80,
               //     ),
               Text("My Application",
-                style: GoogleFonts.dancingScript(
-                  fontSize: 30,
-                  color: Colors.white,
-                  fontWeight:FontWeight.bold
-                 )
+                  style: GoogleFonts.dancingScript(
+                      fontSize: 30,
+                      color: Colors.white,
+                      fontWeight:FontWeight.bold
+                  )
                 // TextStyle(
                 //     fontSize: 40,
                 //     fontStyle: FontStyle.italic,
@@ -64,4 +81,5 @@ class SplashPage extends StatelessWidget {
       ),
     );
   }
+
 }
